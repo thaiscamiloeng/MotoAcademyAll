130b7e9c1f03dc19e5e69f8a28bcc6fcb409c793
Thais Camilo <thaiscamilo.eng@gmail.com>
2023-05-03 20:05:19 -0400

    Sensor

---
+++ Sensor/app/src/main/java/com/thais/sensor/MainActivity.java
@@ -0,0 +1,53 @@
+package com.thais.sensor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    TextView txtP;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txtP=findViewById(R.id.txtP);
+        SensorManager sensorManager=(SensorManager) getSystemService(SENSOR_SERVICE);
+        if(sensorManager!=null){
+            Sensor proximitySensor=sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+            if(proximitySensor!=null){
+                sensorManager.registerListener(this,proximitySensor,SensorManager.SENSOR_DELAY_NORMAL);
+            }
+        }else{
+            Toast.makeText(this, "Sensor não encontrado", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        if(sensorEvent.sensor.getType()==Sensor.TYPE_PROXIMITY){
+            String sensorValue="Proximity Value: "+sensorEvent.values[0];
+            txtP.setText(sensorValue);
+            if(sensorEvent.values[0]>0){
+                Toast.makeText(this, "O objeto está longe", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                Toast.makeText(this, "O objeto está próximo", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+}
